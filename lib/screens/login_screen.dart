8ead62b52b0a1a9846af66f307fd432b82bca7a5
pdromarks <pdro0214@gmail.com>
2025-04-03 13:22:02 -0300

    Correção responsividade

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -3,28 +3,23 @@ import '../widgets/custom_tf.dart';
 import '../widgets/custom_btn.dart';
 import '../theme/theme.dart';
 
+
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    // Obtém o tamanho da tela para responsividade
     final Size screenSize = MediaQuery.of(context).size;
     final double width = screenSize.width;
     final double height = screenSize.height;
+    final double iconSize = ((height < width ? height : width) * 0.05).clamp(10, 20);
     
-    // Determina se é desktop ou mobile
     final bool isDesktop = width > 800;
     final bool isTablet = width > 600 && width <= 800;
-    
-    // Implementação de fontes responsivas usando MediaQuery
-    // Calculadas como porcentagem da largura da tela
     final double titleFontSize = width * (isDesktop ? 0.02 : isTablet ? 0.03 : 0.05);
     final double regularFontSize = width * (isDesktop ? 0.015 : isTablet ? 0.02 : 0.04);
     final double buttonFontSize = width * (isDesktop ? 0.016 : isTablet ? 0.022 : 0.042);
     final double smallFontSize = width * (isDesktop ? 0.012 : isTablet ? 0.018 : 0.035);
-    
-    // Calcula tamanhos proporcionais
-    final double logoHeight = height * 0.15;
+    final double logoHeight = height * 0.25;
     final double verticalSpacing = height * 0.015;
     final double cardWidth = isDesktop ? width * 0.4 : isTablet ? width * 0.7 : width * 0.9;
     
@@ -54,7 +49,7 @@ class LoginScreen extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
                     SizedBox(
-                      height: height * 0.15,
+                      height: logoHeight,
                       child: Image.asset('assets/images/emsalamais.png'),
                     ),
                     SizedBox(height: height * 0.04),
@@ -64,26 +59,28 @@ class LoginScreen extends StatelessWidget {
                       borderColor: AppColors.ciano,
                       labelColor: AppColors.ciano,
                       prefixIcon: Icons.email,
+                      iconSize: iconSize,
                       iconColor: AppColors.ciano,
-                      width: isDesktop ? width * 0.35 : null,
-                      height: height * 0.08 ,
+                      width: isDesktop ? width * 0.28 : null,
+                      height: height * 0.065,
                       maxWidth: 400,
                       fontSize: regularFontSize,
                     ),
                     SizedBox(height: verticalSpacing),
-                    // Campo de senha com ícone de cadeado
                     CustomTextField(
                       label: 'Senha',
                       isPassword: true,
                       borderColor: AppColors.ciano,
                       labelColor: AppColors.ciano,
                       prefixIcon: Icons.lock,
+                      iconSize: iconSize,
                       iconColor: AppColors.ciano,
-                      width: isDesktop ? width * 0.35 : null,
-                      height: height * 0.08,
+                      width: isDesktop ? width * 0.28 : null,
+                      height: height * 0.065, 
                       maxWidth: 400,
                       fontSize: regularFontSize,
                     ),
+                    SizedBox(height: 25),
                     Align(
                       alignment: Alignment.center,
                       child: TextButton(
@@ -105,8 +102,8 @@ class LoginScreen extends StatelessWidget {
                       onPressed: () {},
                       backgroundColor: AppColors.ciano,
                       width: isDesktop ? width * 0.25 : isTablet ? width * 0.35 : width * 0.45,
-                      maxWidth: 200,
-                      height: height * 0.08,
+                      maxWidth: 180,
+                      height: height * 0.065,
                       fontSize: buttonFontSize,
                     ),
                   ],
